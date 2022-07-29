1530ab1b314864a6fed345c6c2b2bc16ad22d33b
Greidy Peña <greidykp@gmail.com>
2022-07-29 12:39:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-4xl">
+        APP
+    </div>
+  )
+}
+
+export default App
